e9355ffee5aaa840300aab55515424a268ad2824
przemyslaw-junior <78381232+przemyslaw-junior@users.noreply.github.com>
2021-02-23 19:31:14 +0100

    Update palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -4,10 +4,10 @@ def palindrome(lista):
     wynik = list()
     for text in lista:
         if text[::-1] == text:
-            wynik.append(text)
-            
-       
-    return 'palindromy', wynik 
+            wynik.append(text)   
+    return 'palindromy', wynik
+
 zdanie = input('podaj zdanie: ') 
 zdanie = zdanie.split()
+
 print(palindrome(zdanie))
